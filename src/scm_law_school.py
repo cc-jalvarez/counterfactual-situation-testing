efdbb283de906d392e5a0ea9f908c9b5e99e714b
jalvarez <jose.alvarez@sns.it>
2022-05-24 17:15:55 +0200

    added and tested generate_scfs method

--- src/scm_law_school.py
+++ src/scm_law_school.py
@@ -7,19 +7,19 @@ class LawSchool(SCM):
     def __init__(self,
                  list_edges_w_weights: List[Tuple[str, str, int or float]],
                  end_vars: List[str],
-                 exo_vars: List[str],
+                 exo_vars: List[str] = None,
                  data: DataFrame = None, ):
         super(LawSchool, self).__init__(edge_list=list_edges_w_weights, )
         self.end_vars = end_vars
         self.exo_vars = exo_vars
-        self.SEM = None
         self.data = data
+        self.SEM = None
         self.scfs = None
 
     def define_sem(self) -> Dict:
         """ Define your set of structural equation model (SEM) as lambda row: df_var(row[x1],...,row[xj]) """
         if self.SEM:
-            print("class instance already have a structural equation model dict; overwrite it via 'SEM' if needed")
+            print("class instance already has a structural equation model dict; overwrite it via 'SEM' if needed")
         else:
             self.SEM = dict.fromkeys(self.end_vars)
             print("introduce the structural equation model as a dict via 'SEM'")
@@ -63,16 +63,18 @@ class LawSchool(SCM):
 
         # do: {'var_to_intervene': 'intervention'}
         for do_var in do.keys():
-            print(f'do({do_var}={do[do_var]}')
+            print(f'do({do_var}={do[do_var]})')
             df[f'org_{do_var}'] = df[do_var].copy()
             df[do_var] = do[do_var]
-        # run SEM model on intervened data
-        df = self.run_model(data=df, type='scf', update_data=False)
 
+        # run SEM model on intervened data
+        df = self.run_sem(data=df, type='scf', update_data=False)
+
+        # store the counterfactuals by do-type
         self.scfs[do_desc] = df
 
         return df
 
-
-    # todo: generate model
-    # todo: generate scf
+#
+# EOF
+#
