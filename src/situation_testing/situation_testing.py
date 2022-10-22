8b6d51a3bdb85a27836dbb103007fc13a3d0a2d9
jalvarez <jose.alvarez@sns.it>
2022-10-22 19:55:56 +0200

    new ST folder

---
+++ src/situation_testing/situation_testing.py
@@ -0,0 +1,45 @@
+
+from pandas import DataFrame
+from typing import List, Dict
+
+# local
+from _distance_functions import _kdd2011dist
+
+
+class SituationTesting:
+    def __init__(self, df: DataFrame, cf_df: DataFrame = None,
+                 nominal_atts: List[str] = None, continuous_atts: List[str] = None, ordinal_atts: List[str] = None):
+
+        # to avoid mutable objects as default arguments
+        nominal_atts = [] if nominal_atts is None else nominal_atts
+        continuous_atts = [] if continuous_atts is None else continuous_atts
+        ordinal_atts = [] if ordinal_atts is None else ordinal_atts
+
+        # set class baseline parameters
+        self.nominal_atts = nominal_atts
+        self.continuous_atts = continuous_atts
+        self.ordinal_atts = ordinal_atts
+        self.all_atts = {'nominal_atts': self.nominal_atts,
+                         'continuous_atts': self.nominal_atts,
+                         'ordinal_atts': self.ordinal_atts}
+        # store positions of attributes
+        cols = list(df.columns)  # cf_df by construction has the same columns
+        self.nominal_atts_pos = [cols.index(c) for c in nominal_atts]
+        self.continuous_atts_pos = [cols.index(c) for c in continuous_atts]
+        self.ordinal_atts_pos = [cols.index(c) for c in ordinal_atts]
+
+        # todo
+        # self.natts = len(continuous_atts) + len(nominal_atts) + len(ordinal_atts)
+        # # statistics of continuous features
+        # self.means = {c: df[c].mean() for c in continuous_atts}
+        # self.stds = {c: df[c].std() for c in continuous_atts}
+        # # statistics of ordinal features
+        # self.nofvalues = {c: (df[c].nunique() - 1) for c in ordinal_atts}
+        # # positions of features (for future usage)
+        # cols = list(df.columns)
+        #
+        # self.stds_pos = {self.continuous_atts_pos[i]: self.stds[c] for i, c in enumerate(continuous_atts)}
+        # self.nofvalues_pos = {self.ordinal_atts_pos[i]: self.nofvalues[c] for i, c in enumerate(ordinal_atts)}
+
+
+
