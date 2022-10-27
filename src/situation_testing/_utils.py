36dcd7c0ff48b7f9f9afd719014d0375b355c453
jalvarez <jose.alvarez@sns.it>
2022-10-27 20:31:58 +0200

    get target neg val

--- src/situation_testing/_utils.py
+++ src/situation_testing/_utils.py
@@ -1,8 +1,15 @@
 from typing import Dict
 
 
-def get_prot_value(sensitive_val: Dict):
+def get_pro_value(sensitive_val: Dict):
     if 'protected' in sensitive_val.keys():
         return sensitive_val['protected']
     else:
         raise ValueError("missing 'protected' key in dict value for the sensitive attribute(s)")
+
+
+def get_neg_value(target_val: Dict):
+    if 'negative' in target_val.keys():
+        return target_val['negative']
+    else:
+        raise ValueError("missing 'negative' key in dict value for the target attribute")
