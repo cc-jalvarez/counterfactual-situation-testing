22e5005a738c5e263b81359099ace390357bac52
jalvarez <jose.alvarez@sns.it>
2022-10-31 11:42:13 +0100

    new method: setup_baseline

--- src/situation_testing/situation_testing.py
+++ src/situation_testing/situation_testing.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Tuple
 from pandas import DataFrame
+from sklearn import preprocessing
 import heapq
 import math
 import pandas as pd
@@ -13,45 +14,44 @@ __DISTANCES__ = {'kdd2011': kdd2011dist}
 
 
 class SituationTesting:
-    def __init__(self,
-                 df: DataFrame, cf_df: DataFrame = None,
-                 nominal_atts: List[str] = None, continuous_atts: List[str] = None, ordinal_atts: List[str] = None,
-                 *args, **kwargs
-                 ):
+    def __init__(self, *args, **kwargs):
+        self.df = None
+        self.cf_df = None
+        self.nominal_atts = None
+        self.continuous_atts = None
+        self.ordinal_atts = None
+        self.all_atts = None
+        self.relevant_atts = None
+        self.nominal_atts_pos = None
+        self.continuous_atts_pos = None
+        self.ordinal_atts_pos = None
+        self.natts = None
 
-        # standard k-NN ST vs counterfactual k-NN ST
-        self.df = df  # todo: why feed the data at all here?
-        self.cf_df = cf_df  # todo: should we check the two?
-        # to avoid mutable objects as default arguments
-        nominal_atts = [] if nominal_atts is None else nominal_atts
-        continuous_atts = [] if continuous_atts is None else continuous_atts
-        ordinal_atts = [] if ordinal_atts is None else ordinal_atts
-
-        # set class baseline parameters
-        self.nominal_atts = nominal_atts
-        self.continuous_atts = continuous_atts
-        self.ordinal_atts = ordinal_atts
-        self.all_atts = {'nominal_atts': self.nominal_atts, 'continuous_atts': self.nominal_atts, 'ordinal_atts': self.ordinal_atts}
+    def setup_baseline(self, df: DataFrame, cf_df: DataFrame = None,
+                       nominal_atts: List[str] = None,
+                       continuous_atts: List[str] = None,
+                       ordinal_atts: List[str] = None,
+                       ):
+        self.df = df
+        self.cf_df = cf_df
+        self.nominal_atts = [] if nominal_atts is None else nominal_atts
+        self.continuous_atts = [] if continuous_atts is None else continuous_atts
+        self.ordinal_atts = [] if ordinal_atts is None else ordinal_atts
         self.relevant_atts = self.nominal_atts + self.continuous_atts + self.ordinal_atts
-        print('relevant attributes: {rel_att}'.format(rel_att=self.relevant_atts))
+        self.all_atts = {'nominal_atts': self.nominal_atts,
+                         'continuous_atts': self.nominal_atts,
+                         'ordinal_atts': self.ordinal_atts}
+        cols = list(df.columns)
+        self.nominal_atts_pos = [cols.index(c) for c in nominal_atts]
+        self.continuous_atts_pos = [cols.index(c) for c in continuous_atts]
+        self.ordinal_atts_pos = [cols.index(c) for c in ordinal_atts]
+        self.natts = len(continuous_atts) + len(nominal_atts) + len(ordinal_atts)
 
-        # todo
-        # # store positions of attributes
-        # cols = list(df.columns)  # cf_df by construction has the same columns as df
-        # self.nominal_atts_pos = [cols.index(c) for c in nominal_atts]
-        # self.continuous_atts_pos = [cols.index(c) for c in continuous_atts]
-        # self.ordinal_atts_pos = [cols.index(c) for c in ordinal_atts]
-        # self.natts = len(continuous_atts) + len(nominal_atts) + len(ordinal_atts)
-        # # statistics of continuous features
-        # self.means = {c: df[c].mean() for c in continuous_atts}
-        # self.stds = {c: df[c].std() for c in continuous_atts}
-        # # statistics of ordinal features
-        # self.nofvalues = {c: (df[c].nunique() - 1) for c in ordinal_atts}
-        # # positions of features (for future usage)
-        # cols = list(df.columns)
-        #
-        # self.stds_pos = {self.continuous_atts_pos[i]: self.stds[c] for i, c in enumerate(continuous_atts)}
-        # self.nofvalues_pos = {self.ordinal_atts_pos[i]: self.nofvalues[c] for i, c in enumerate(ordinal_atts)}
+        # normalize the data
+        scaler = preprocessing.StandardScaler()
+        self.df[self.continuous_atts] = scaler.fit_transform(self.df[self.continuous_atts])
+        if self.cf_df:
+            self.cf_df[self.continuous_atts] = scaler.fit_transform(self.cf_df[self.continuous_atts])
 
     def top_k(self, t, tset, k: int, distance: str, max_d: float = None) -> List[Tuple[float, int]]:
         """
@@ -88,8 +88,9 @@ class SituationTesting:
     #     pass
 
     def get_k_neighbors(self,
-                        target_att: str, target_val: Dict, sensitive_att: str or List[str], sensitive_val: Dict, k: int,
-                        distance: str = 'kdd2011', max_d: float = None, relevant_atts: List[str] = None,
+                        target_att: str, target_val: Dict,
+                        sensitive_att: str or List[str], sensitive_val: Dict,
+                        k: int, distance: str = 'kdd2011', max_d: float = None,
                         return_counterfactual_fairness: bool = False, return_neighbors: bool = False):
 
         # output:
@@ -103,8 +104,6 @@ class SituationTesting:
             res_cf = pd.Series(np.zeros(len(self.df)), index=self.df.index)
         else:
             res_cf = None
-        # relevant decision-making attributes doesn't include A nor y
-        self.relevant_atts = relevant_atts if relevant_atts else self.relevant_atts
 
         # todo: atm, only |A|=1
         # gather info for control (ctr) and test (tst) groups
@@ -118,17 +117,17 @@ class SituationTesting:
         sensitive_val = get_pro_value(sensitive_val)
         sensitive_set = self.df[sensitive_att] == sensitive_val  # returns a pd.Series of booleans
         # define search spaces
-        ctr_search = self.df[sensitive_set].copy()
-        tst_search = self.df[~sensitive_set].copy()
+        ctr_search = self.df[sensitive_set, self.relevant_atts].copy()
+        tst_search = self.df[~sensitive_set, self.relevant_atts].copy()
         # find idx for control and test neighborhoods
         for c in self.df[sensitive_set].index.to_list():
-            ctr_k = self.top_k(self.df.loc[c, ], ctr_search[self.relevant_atts], k + 1, distance, max_d)
+            ctr_k = self.top_k(self.df.loc[c, ], ctr_search, k + 1, distance, max_d)
             if self.cf_df:
                 # cfST: draw test center from counterfactual df
-                tst_k = self.top_k(self.cf_df.loc[c, ], tst_search[self.relevant_atts], k, distance, max_d)
+                tst_k = self.top_k(self.cf_df.loc[c, ], tst_search, k, distance, max_d)
             else:
                 # standard ST: draw test center from factual df
-                tst_k = self.top_k(self.df.loc[c, ], tst_search[self.relevant_atts], k, distance, max_d)
+                tst_k = self.top_k(self.df.loc[c, ], tst_search, k, distance, max_d)
             nn1 = [j for _, j in ctr_k if j != c]  # idx for ctr_k (minus center)
             nn2 = [j for _, j in tst_k]            # idx for tst_k
             p1 = sum(self.df.loc[nn1, ][target_att] == bad_y_val) / len(nn1)
