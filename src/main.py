2d7b55e8f4433e1ef14751874b50c8811fe57eed
jalvarez <jose.alvarez@sns.it>
2022-11-18 16:33:49 +0100

    updated main

--- src/main.py
+++ src/main.py
@@ -20,27 +20,56 @@ feat_rlvt = ['AnnualSalary', 'AccountBalance']
 feat_prot = 'Gender'
 # values for the protected feature: use 'non_protected' and 'protected' accordingly
 feat_prot_vals = {'non_protected': 0, 'protected': 1}
+# k-neighbors
+n = 15
+# significance level
+alpha = 0.05
+# tau deviation
+tau = 0.0
 
-# todo
-org_df = df.copy()
+# --- ST
+test_df = df.copy()
 
 st = SituationTesting()
+st.setup_baseline(test_df, nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
 
-# st.setup_baseline(df=df,
-#                   nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
-# org_df['diff'] = st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
-#                         sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
-#                         k=15)
+test_df['ST'] = st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                       sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                       k=n, alpha=alpha, tau=tau)
+print(test_df[test_df['ST'] > tau].shape[0])
 
-st.setup_baseline(df=df,
-                  cf_df=cf_df,
-                  nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
-org_df['diff'] = st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
-                        sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
-                        k=15,
-                        include_centers=True)
+del test_df
 
-print(org_df.head(5))
+# --- cfST without centers
+test_df = df.copy()
+test_cfdf = cf_df.copy()
 
+# don't include the centers
+cf_st = SituationTesting()
+cf_st.setup_baseline(test_df, test_cfdf, nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
 
+test_df['cfST'] = cf_st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                            sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                            include_centers=False,
+                            k=n, alpha=alpha, tau=tau)
 
+print(test_df[test_df['cfST'] > tau].shape[0])
+
+del test_df
+
+# --- cfST with centers
+test_df = df.copy()
+test_cfdf = cf_df.copy()
+
+# include the centers
+cf_st = SituationTesting()
+cf_st.setup_baseline(test_df, test_cfdf, nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
+
+test_df['cfST'] = cf_st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                            sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                            include_centers=True,
+                            k=n, alpha=alpha, tau=tau)
+
+print(test_df[test_df['cfST'] > tau].shape[0])
+
+del test_df
