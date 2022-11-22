43c2926de87b05eed61540f02cf73949d5ccc283
jalvarez <jose.alvarez@sns.it>
2022-11-22 20:43:02 +0100

    looping k

--- src/main.py
+++ src/main.py
@@ -73,3 +73,54 @@ test_df['cfST'] = cf_st.run(target_att='LoanApproval', target_val={'positive': 1
 print(test_df[test_df['cfST'] > tau].shape[0])
 
 del test_df
+
+for new_k in [15, 30, 50, 100]:
+    print('===> k={k}'.format(k=new_k))
+
+    print('standard ST')
+    test_df = df.copy()
+
+    st = SituationTesting()
+    st.setup_baseline(test_df, nominal_atts=['Gender'], continuous_atts=['AnnualSalary', 'AccountBalance'])
+
+    test_df['ST'] = st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                           sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                           k=new_k, alpha=alpha, tau=tau)
+    print(test_df[test_df['ST'] > tau].shape[0])
+
+    del test_df
+
+    print('counterfactual ST (without centers)')
+    test_df = df.copy()
+    test_cfdf = cf_df.copy()
+
+    cf_st = SituationTesting()
+    cf_st.setup_baseline(test_df, test_cfdf, nominal_atts=['Gender'],
+                         continuous_atts=['AnnualSalary', 'AccountBalance'])
+
+    test_df['cfST'] = cf_st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                                sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                                include_centers=False,
+                                k=new_k, alpha=alpha, tau=tau)
+
+    print(test_df[test_df['cfST'] > tau].shape[0])
+
+    del test_df
+
+    print('counterfactual ST (with centers)')
+    test_df = df.copy()
+    test_cfdf = cf_df.copy()
+
+    # include the centers
+    cf_st = SituationTesting()
+    cf_st.setup_baseline(test_df, test_cfdf, nominal_atts=['Gender'],
+                         continuous_atts=['AnnualSalary', 'AccountBalance'])
+
+    test_df['cfST'] = cf_st.run(target_att='LoanApproval', target_val={'positive': 1, 'negative': -1},
+                                sensitive_att='Gender', sensitive_val={'non_protected': 0, 'protected': 1},
+                                include_centers=True,
+                                k=new_k, alpha=alpha, tau=tau)
+
+    print(test_df[test_df['cfST'] > tau].shape[0])
+
+    del test_df
